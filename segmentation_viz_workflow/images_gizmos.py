5fbda74aa58758654f0b30a05c30082c04ec582a
Aaron Watters <awatters@simonsfoundation.org>
2022-11-15 16:35:02 -0500

    sliders and misc

--- segmentation_viz_workflow/images_gizmos.py
+++ segmentation_viz_workflow/images_gizmos.py
@@ -22,47 +22,136 @@ class CompareTimeStamps:
         self.info_area = Text("Building scaffolding.")
         self.parent_display = ImageAndLabels2d(side, None)
         self.child_display = ImageAndLabels2d(side, None)
-        self.gizmo = Stack([ 
+        self.displays = Stack([ 
             self.title_area,
             self.parent_display.gizmo,
             self.child_display.gizmo,
             self.info_area,
+        ])
+        sliders = self.get_sliders(side)
+        self.gizmo = Shelf([ 
+            self.displays,
+            sliders,
         ])
         # array shape (maxima)
         self.slicing = None
         self.theta = 0
         self.phi = 0
 
+    def get_sliders(self, side):
+        limit = np.pi
+        self.theta_slider = Slider(
+            title="theta", 
+            orientation="vertical",
+            value=0, 
+            minimum=-limit, 
+            maximum=+limit, 
+            step=0.02, 
+            on_change=self.project_and_display)
+        self.theta_slider.resize(height=side)
+        self.phi_slider = Slider(
+            title="theta", 
+            orientation="vertical",
+            value=0, 
+            minimum=-limit, 
+            maximum=+limit, 
+            step=0.02, 
+            on_change=self.project_and_display)
+        self.phi_slider.resize(height=side)
+        # dummy values for now
+        I = 100
+        self.I_slider = RangeSlider(
+            orientation="vertical",
+            minimum=0,
+            maximum=I,
+            low_value=0,
+            high_value=I,
+            step=1,
+            title="I slider",
+            on_change=self.project_and_display,
+        )
+        self.I_slider.resize(height=side)
+        self.J_slider = RangeSlider(
+            orientation="vertical",
+            minimum=0,
+            maximum=I,
+            low_value=0,
+            high_value=I,
+            step=1,
+            title="K slider",
+            on_change=self.project_and_display,
+        )
+        self.J_slider.resize(height=side)
+        self.K_slider = RangeSlider(
+            orientation="vertical",
+            minimum=0,
+            maximum=I,
+            low_value=0,
+            high_value=I,
+            step=1,
+            title="K slider",
+            on_change=self.project_and_display,
+        )
+        self.K_slider.resize(height=side)
+        return [
+            [
+                ["ϕ", self.phi_slider],
+                ["θ", self.theta_slider]
+            ],
+            [ 
+                ["I", self.I_slider],
+                ["J", self.J_slider],
+                ["K", self.K_slider],
+            ]
+        ]
+
     def configure_gizmo(self):
         self.parent_display.configure_gizmo()
         self.child_display.configure_gizmo()
+        self.gizmo.css({"background-color": "#ddd"})
 
     def set_parent_timestamp(self, ordinal):
         ts = self.forest.ordinal_to_timestamp.get(ordinal)
         if ts is None:
             self.info_area.text("No such parent timestamp ordinal: " + repr(ordinal))
         else:
-            self.parent_display.reset(ts, self.forest)
+            self.parent_display.reset(ts, self.forest, self)
 
     def set_child_timestamp(self, ordinal):
         ts = self.forest.ordinal_to_timestamp.get(ordinal)
         if ts is None:
             self.info_area.text("No such child timestamp ordinal: " + repr(ordinal))
         else:
-            self.child_display.reset(ts, self.forest)
+            self.child_display.reset(ts, self.forest, self)
+
+    def project_and_display(self, *ignored):
+        self.theta = self.theta_slider.value
+        self.phi = self.phi_slider.value
+        s = [
+            self.I_slider.values,
+            self.J_slider.values,
+            self.K_slider.values,
+        ]
+        self.slicing = np.array(s, dtype=np.int)
+        self.project2d()
+        self.display_images()
 
     def project2d(self):
         self.parent_display.project2d(self)
         self.child_display.project2d(self)
 
     def display_images(self):
+        self.parent_display.create_mask()
+        self.child_display.create_mask()
+        self.parent_display.load_mask(self.child_display)
+        self.child_display.load_mask(self.parent_display)
         self.parent_display.display_images()
         self.child_display.display_images()
 
     def rotate_image(self, img):
         sl = self.slicing
         simg = img
-        if sl:
+        if sl is not None:
             simg = operations3d.slice3(img, sl)
         buffer = operations3d.rotation_buffer(simg)
         rbuffer = operations3d.rotate3d(buffer, self.theta, self.phi)
@@ -92,8 +181,7 @@ class ImageAndLabels2d:
         self.label_volume = None
         self.reset(timestamp)
 
-    def reset(self, timestamp=None, forest=None):
-        print("resetting", self)
+    def reset(self, timestamp=None, forest=None, comparison=None):
         self.timestamp = timestamp
         #self.color_mapping_array = timestamp.color_mapping_array()
         self.img = None
@@ -107,6 +195,9 @@ class ImageAndLabels2d:
         self.label_volume = None
         self.image_volume = None
         self.volume_shape = None
+        if comparison is not None:
+            assert type(comparison) is CompareTimeStamps
+        self.comparison = comparison
         if forest is not None and timestamp is not None:
             ordinal = timestamp.ordinal
             label_volume = forest.load_labels_for_timestamp(ordinal)
@@ -155,11 +246,18 @@ class ImageAndLabels2d:
             self.info("No labels to select")
             return 
         label = labels[row, column]
-        self.info("clicked label: " + repr(label))
+        node = None
+        self.info("clicked label: %s for node %s" % (label, node))
         if label:
             node = self.timestamp.label_to_node[label]
+            self.info("clicked label: %s for node %s" % (label, node))
             self.focus_label = label
             self.focus_color = node.color_array
+        else:
+            self.focus_label = None
+            self.focus_color = None
+        if self.comparison is not None:
+            self.comparison.display_images()
 
     def create_mask(self):
         label = self.focus_label
@@ -200,7 +298,8 @@ class ImageAndLabels2d:
             (self.compare_mask, self.compare_color)]:
             if mask is not None:
                 assert color is not None, "no color for mask?"
-                img = colorizers.overlay_color(img, mask, color)
+                #assert img.shape == mask.shape, "shapes don't match: " + repr([img.shape, mask.shape])
+                img = colorizers.overlay_color(img, mask, color, center=True)
         img = self.pad_image(img)
         labels = self.pad_image(labels)
         self.image_display.change_array(img)
