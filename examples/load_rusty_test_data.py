b290e2cb01ea3194bb35af8adab6508ed77b03e2
Aaron Watters <awatters@simonsfoundation.org>
2022-11-18 13:56:13 -0500

    real rusty paths

--- examples/load_rusty_test_data.py
+++ examples/load_rusty_test_data.py
@@ -12,19 +12,26 @@ fn = "Combined.json"
 json_ob = json.load(open(fn))
 F = lineage_forest.make_forest_from_haydens_json_graph(json_ob)
 
-trivialize = True
+trivialize = False
 
 if trivialize:
     F.use_trivial_null_loaders()
 else:
-    assert trivialize, "non-trivial implementation is not finished."
-    img_folder = "/Users/awatters/test/mouse_embryo_images/"
-    image_pattern = img_folder + "klbOut_Cam_Long_%(ordinal)05d.crop.klb"
-    label_pattern = img_folder + "klbOut_Cam_Long_%(ordinal)05d.crop_cp_masks.klb"
+    #assert trivialize, "non-trivial implementation is not finished."
+    #img_folder = "/Users/awatters/test/mouse_embryo_images/"
+    #image_pattern = img_folder + "klbOut_Cam_Long_%(ordinal)05d.crop.klb"
+    #label_pattern = img_folder + "klbOut_Cam_Long_%(ordinal)05d.crop_cp_masks.klb"
+    label_pattern = "/mnt/home/awatters/ceph/220827_stack1/Segmentation/stack_1_channel_0_obj_left/cropped/klbOut_Cam_Long_%(ordinal)05d.crop_cp_masks.klb"
+    image_pattern = "/mnt/home/awatters/ceph/220827_stack1/Segmentation/stack_1_channel_0_obj_left/membrane/cropped/klbOut_Cam_Long_%(ordinal)05d.crop.klb"
     F.load_klb_using_file_patterns(
             image_pattern=image_pattern,
             label_pattern=label_pattern,
         )
+    test_ordinal = 29
+    img = F.load_image_for_timestamp(test_ordinal)
+    assert img is not None, "Could not load image for: " + repr(test_ordinal)
+    lab = F.load_labels_for_timestamp(test_ordinal)
+    assert lab is not None, "Could not load image for: " + repr(test_ordinal)
 
 viewer = images_gizmos.LineageViewer(F, 400)
 
